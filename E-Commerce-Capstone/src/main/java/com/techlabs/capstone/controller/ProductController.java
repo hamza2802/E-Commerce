82324b2bf13c45aca2d11466e55fdf6e9afcc1ec
hamza <ansarihamza2802@gmail.com>
2025-01-03 17:58:15 +0530

    hamza 5:58 pm

--- E-Commerce-Capstone/src/main/java/com/techlabs/capstone/controller/ProductController.java
+++ E-Commerce-Capstone/src/main/java/com/techlabs/capstone/controller/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -87,6 +88,13 @@ public class ProductController {
 	        } catch (RuntimeException e) {
 	            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);  
 	        }
+	    }
+	 
+	  @DeleteMapping("/{productId}")
+	    @PreAuthorize("hasRole('ADMIN')") 
+	    public ResponseEntity<Void> deleteProduct(@PathVariable int productId) {
+	        productService.deleteProduct(productId);
+	        return new ResponseEntity<>(HttpStatus.NO_CONTENT); 
 	    }
 
 }
